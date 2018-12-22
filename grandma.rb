328579e3806cf961d98751e87b43f3d8c4648eea
Manuela  Sanchez <m.sanchez947@hotmail.com>
2018-12-22 01:51:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,6 @@ def speak_to_grandma(phrase)
   elsif "#{phrase}".uppercase
     return "NO, NOT SINCE 1938!"
   else
-    return "I LOVE YOU TOO PUMPKIN!"
+    return "HUH?! SPEAK UP, SONNY!"
 end
 end
